18bae3b0d852a0857a9207ff25e331e88792c8fb
soarthec <soarthec@macbook-air-de-benoit.home>
2019-08-16 10:32:23 +0200

    début config

---
+++ Config.py
@@ -0,0 +1,10 @@
+# CREATE DATABASE THESAURUS_DB
+# USE THESAURUS_DB
+DATABASE = "THESAURUS_DB"
+
+# CREATE USER 'thesaurus'@'localhost' IDENTIFIED BY 'thesaurus_pwd';
+USER = "thesaurus"
+PASSWORD = "thesaurus_pwd"
+HOST = "localhost"
+
+TABLES = {}
